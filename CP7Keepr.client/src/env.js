0ac99841b8494dcc805f295d9c69b988725c5cfc
Kendall Powell <kendalpowel@hotmail.com>
2023-01-27 12:28:24 -0700

    env hopefully set up right

--- CP7Keepr.client/src/env.js
+++ CP7Keepr.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
+export const baseURL = dev ? 'https://localhost:7045' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-beesng7oajjsxf2g.us.auth0.com'
+export const clientId = 'pxP5wIDdSjjVUOBVgFjDxYGma0fmEVXZ'
+export const audience = 'https://simpleuse'
